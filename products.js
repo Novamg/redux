6f3db05ea9168483ec7b1c5a14d7f8e655eb3a51
Sara George <novamg14022003@gmail.com>
2023-01-11 15:42:43 +0530

    FIRST commit

---
+++ products.js
@@ -0,0 +1,36 @@
+export const products = [
+
+    {
+        id: 1,
+        name: 'Diary Of A Wimpy Kid',
+        price: 80,
+        image:require('./img1.jpeg')
+
+    },
+    {
+        id: 2,
+        name: '400 Days',
+        price: 30,
+        image:require('./img2.jpeg')
+    },
+    {
+        id: 3,
+        name: '2 States',
+        price: 50,
+        image:require('./img3.jpeg')
+    },
+    {
+        id: 4,
+        name: 'November 9',
+        price: 100,
+        image:require('./img4.jpeg')
+ 
+    },
+    {
+        id: 5,
+        name: 'The ABC Murders',
+        price: 150,
+        image:require('./img5.jpeg')
+    },
+
+]
